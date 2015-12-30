6ef4aad6d0f6e4724f62d1e736732d5baa8f377f
MH_VANESSA <sapiens12@nate.com>
2015-12-30 14:15:24 +0900

    마이페이지 메인

---
+++ oozoobox/shop/mypage01.php
@@ -0,0 +1,55 @@
+<?php
+include_once('./_common.php');
+
+if (!$is_member)
+    goto_url(G5_BBS_URL."/login.php?url=".urlencode(G5_SHOP_URL."/mypage.php"));
+
+// 쿠폰
+$cp_count = 0;
+$sql = " select cp_id
+            from {$g5['g5_shop_coupon_table']}
+            where mb_id IN ( '{$member['mb_id']}', '전체회원' )
+              and cp_start <= '".G5_TIME_YMD."'
+              and cp_end >= '".G5_TIME_YMD."' ";
+$res = sql_query($sql);
+
+for($k=0; $cp=sql_fetch_array($res); $k++) {
+    if(!is_used_coupon($member['mb_id'], $cp['cp_id']))
+        $cp_count++;
+}
+
+$mb_homepage = set_http(get_text(clean_xss_tags($member['mb_homepage'])));
+$mb_profile = ($member['mb_profile']) ? conv_content($member['mb_profile'],0) : '';
+$mb_signature = ($member['mb_signature']) ? apms_content(conv_content($member['mb_signature'], 1)) : '';
+
+
+
+$g5['title'] = get_text($member['mb_name']).'님 마이페이지';
+include_once('./_head.php');
+
+$skin_path = $member_skin_path;
+$skin_url = $member_skin_url;
+?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+4567890
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<?php  include_once('./_tail.php'); ?>
