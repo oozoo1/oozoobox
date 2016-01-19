4be49646e90cfbe70e4ef8565d83808671c0bb02
HeoGeol <heogeol@naver.com>
2016-01-19 16:26:13 +0900

    로그인 체크 번역 및 메일 인정 수정

--- oozoobox/bbs/login_check.php
+++ oozoobox/bbs/login_check.php
@@ -1,13 +1,13 @@
 <?php
 include_once('./_common.php');
 
-$g5['title'] = "로그인 검사";
+$g5['title'] = "登录检测";
 
 $mb_id       = trim($_POST['mb_id']);
 $mb_password = trim($_POST['mb_password']);
 
 if (!$mb_id || !$mb_password)
-    alert('회원아이디나 비밀번호가 공백이면 안됩니다.');
+    alert('账号或密码 不能输入空白.');
 
 $mb = get_member($mb_id);
 
@@ -15,23 +15,23 @@ $mb = get_member($mb_id);
 // 회원아이디를 입력해 보고 맞으면 또 비밀번호를 입력해보는 경우를 방지하기 위해서입니다.
 // 불법사용자의 경우 회원아이디가 틀린지, 비밀번호가 틀린지를 알기까지는 많은 시간이 소요되기 때문입니다.
 if (!$mb['mb_id'] || !check_password($mb_password, $mb['mb_password'])) {
-    alert('가입된 회원아이디가 아니거나 비밀번호가 틀립니다.\\n비밀번호는 대소문자를 구분합니다.');
+    alert('没有注册会员的账号或密码错误.\\n密码请区分大小写.');
 }
 
 // 차단된 아이디인가?
 if ($mb['mb_intercept_date'] && $mb['mb_intercept_date'] <= date("Ymd", G5_SERVER_TIME)) {
-    $date = preg_replace("/([0-9]{4})([0-9]{2})([0-9]{2})/", "\\1년 \\2월 \\3일", $mb['mb_intercept_date']);
-    alert('회원님의 아이디는 접근이 금지되어 있습니다.\n처리일 : '.$date);
+    $date = preg_replace("/([0-9]{4})([0-9]{2})([0-9]{2})/", "\\1年 \\2月 \\3日", $mb['mb_intercept_date']);
+    alert('您的账号已被管理员屏蔽.\n屏蔽日 : '.$date);
 }
 
 // 탈퇴한 아이디인가?
 if ($mb['mb_leave_date'] && $mb['mb_leave_date'] <= date("Ymd", G5_SERVER_TIME)) {
-    $date = preg_replace("/([0-9]{4})([0-9]{2})([0-9]{2})/", "\\1년 \\2월 \\3일", $mb['mb_leave_date']);
-    alert('탈퇴한 아이디이므로 접근하실 수 없습니다.\n탈퇴일 : '.$date);
+    $date = preg_replace("/([0-9]{4})([0-9]{2})([0-9]{2})/", "\\1年 \\2月 \\3日", $mb['mb_leave_date']);
+    alert('你的账号已申请 注销.\n注销日 : '.$date);
 }
 
 if ($config['cf_use_email_certify'] && !preg_match("/[1-9]/", $mb['mb_email_certify'])) {
-    confirm("{$mb['mb_email']} 메일로 메일인증을 받으셔야 로그인 가능합니다. 다른 메일주소로 변경하여 인증하시려면 취소를 클릭하시기 바랍니다.", G5_URL, G5_BBS_URL.'/register_email.php?mb_id='.$mb_id);
+		alert("{$mb['mb_email']} 您的账号 还没有通过 邮箱验证 请确认邮箱是否填写正确. 如有错误请 更换您的邮箱.", G5_BBS_URL.'/register_email.php?mb_id='.$mb_id);
 }
 
 @include_once($member_skin_path.'/login_check.skin.php');
