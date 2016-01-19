b30d055185f5a07e27e9aa05bd31cbf4e6e7cbdd
HeoGeol <heogeol@naver.com>
2016-01-19 17:26:26 +0900

    로그인 페이지 디자인 변경 비밀번호찾기 디자인변경

--- oozoobox/skin/member/basic/login.skin.php
+++ oozoobox/skin/member/basic/login.skin.php
@@ -14,88 +14,108 @@ function chkReset(f)
     document.getElementById("pw2").style.display = "";
 }
 </script>
-<!-- 登录开始 { --><style type="text/css">
+<!-- 登录开始 { -->
+<style type="text/css">
 <!--
+html { overflow-y: scroll; }
 body,td,th {
-	font-size: 12px;
+	font-size: 12px; font-family:微软雅黑;
 }
 body {
 	margin-left: 0px;
 	margin-top: 0px;
 	margin-right: 0px;
 	margin-bottom: 0px;
-	 background-color:#f6c801;
+	background-image: url(<?=$skin_url?>/images/login_bg.jpg);
+	background-repeat: repeat-x;
+	background-color: #eeeeee;
 }
-
+/*** 기본버튼 **/
+.btn.btn-color { 
+	border: 1px solid #f47a22; 
+	border-image: none; 
+	color: rgb(255, 255, 255) !important; 
+	background-color: #f5944f; 
+	background-image: none;
+	padding:10px 20px 10px 20px;
+}
+.btn.active.btn-color, .btn.btn-color:hover, .btn.btn-color:focus, .btn.btn-color:active { 
+	border-color: #f47a22; 
+	color: rgb(255, 255, 255) !important; 
+	background-color: #f47a22; 
+	background-image: none; 
+	padding:10px 20px 10px 20px;
+}
+a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:hover {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
 -->
 </style>
-
-<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+<table width="990" border="0" cellspacing="0" cellpadding="0" align="center">
+<form name="flogin" action="<?php echo $login_action_url ?>" onsubmit="return flogin_submit(this);" method="post">
+<input type="hidden" name="url" value='<?php echo $login_url ?>'>
   <tr>
-    <td height="760" style="background:url(/images/login_bg.png) no-repeat center bottom;">    
-        <table width="914" border="0" cellspacing="0" cellpadding="0" align="center">          
-          <tr>
-            <td width="624" bgcolor="#ffffff">&nbsp;</td>
-      <td width="380" height="487" valign="top" bgcolor="#ffffff" style="position:static; box-shadow:1px 1px 1px 1px #cccccc; padding:38px;">
-                <table width="300" border="0" cellspacing="0" cellpadding="0" align="center">
-                <form name="flogin" action="<?php echo $login_action_url ?>" onsubmit="return flogin_submit(this);" method="post">
-                <input type="hidden" name="url" value='<?php echo $login_url ?>'>
-                  <tr>
-                    <td width="100" height="24" style="color:#555; font-size:1.3em;">欢迎登录</td>
-                    <td width="20"></td>
-                    <td><div style="border-bottom:solid 1px #555; width:100%;"></div></td>
-                  </tr>
-                  <tr>
-                    <td height="22" colspan="3"></td>
-                  </tr>
-                  <tr>
-                    <td height="41" colspan="3">
-                    <input id="mb_id" name="mb_id" type="text" class="input" value='请输入用户名' style="border:solid 1px #dedede; background-image:url(/images/login_id_bg.png); color:#aaaaaa; width:300px; height:40px; padding-left:50px;"  onMouseOver='chkReset(this.form);' onFocus='chkReset(this.form);'/>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td height="27" colspan="3"></td>
-                  </tr>
-                  <tr>
-                    <td height="41" colspan="3" id=pw1>
-                    <input id="mb_password" name="mb_password" type="text" class="input" value='请输入密码' style="border:solid 1px #dedede; background-image:url(/images/login_pw_bg.png); color:#aaaaaa; width:300px; height:40px; padding-left:50px;" onMouseOver='chkReset(this.form);' onfocus='chkReset(this.form);'/>
-                    </td>
-                    <td height="41" colspan="3" id=pw2 style='display:none;'>
-                    <input id="mb_password" name="mb_password" type="password" class="input" style="border:solid 1px #dedede; background-image:url(/images/login_pw_bg.png); color:#aaaaaa; width:300px; height:40px; padding-left:50px;"/>
-                    </td>
-                  </tr>                  
-                  <tr>
-                    <td height="27" colspan="3"></td>
-                  </tr>
-                  <tr>
-                    <td height="41" colspan="3"><input type="submit" value="  登  录  " class="left"  style="background-color:#fd5c02; width:300px; height:40px; border:0; font-weight:bold; color:#fff;"/></td>
-                  </tr>
-                  </form>
-                </table>
-                <table width="300" border="0" cellspacing="0" cellpadding="0">
-                  <tr>
-                    <td align="left" height="45"><a href="./register.php" style="color:#4d4b4c;">还没有帐号?立即注册</a></td>
-                    <td align="right"><a href="<?php echo G5_BBS_URL ?>/password_lost.php" style="color:#4d4b4c;" target="_blank" >忘记密码</a></td>
-                  </tr>
-                </table>                
-            </td>
-          </tr>
-        </table>
+    <td height="162" valign="top" style="padding-top:20px;"><a href="/"><img src="<?=$skin_url?>/images/join_logo.png" border="0"></a></td>
+  </tr>
+  <tr>
+    <td height="438" valign="top" background="<?=$skin_url?>/images/join_login_bg.png" style="padding:23px;">
+      <table width="944" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td width="574" height="390" valign="top"><a href="/shop/list.php?ca_id=10" target="_blank"><img src="<?=$skin_url?>/images/login_ad.png" border="0"></a></td>
+          <td width="370" valign="top">
+              <table width="300" border="0" cellspacing="0" cellpadding="0" align="center">
+                <tr>
+                  <td><font style="font-family:微软雅黑; font-size:18px; color:#888888; font-weight:bold;">会员登录</font></td>
+                  <td align="right"><a href="/bbs/register.php"><img src="<?=$skin_url?>/images/join_bt_join.png"></a></td>
+                </tr>
+              </table>
+              <table width="300" border="0" cellspacing="0" cellpadding="0" align="center">
+                <tr>
+                  <td height="45"></td>
+                </tr>
+                <tr>
+                  <td>
+                    <table width="300" border="0" cellspacing="0" cellpadding="0">
+                        <tr>
+                          <td height="50"><input name="mb_id" type="text" class="input" style="border:solid 1px #dedede; background-image:url(/images/login_id_bg.png); color:#aaaaaa; width:300px; height:40px; padding-left:50px;"/></td>
+                        </tr>
+                        <tr>
+                          <td height="50"><input name="mb_password" type="password" class="input" style="border:solid 1px #dedede; background-image:url(/images/login_pw_bg.png); color:#aaaaaa; width:300px; height:40px; padding-left:50px;"/></td>
+                        </tr>
+                        <tr>
+                          <td height="70"><input type="submit" value="  登  录  " class="left"  style="background-color:#fd5c02; width:300px; height:40px; border:0; font-weight:bold; color:#fff;"/></td>
+                        </tr>
+                        <tr>
+                          <td height="70">
+                            <table width="300" border="0" cellspacing="0" cellpadding="0">
+                              <tr>
+                                <td align="left" height="45"><a href="./register.php" style="color:#4d4b4c;">还没有帐号?立即注册</a></td>
+                                <td align="right"><a href="<?php echo G5_BBS_URL ?>/password_lost.php" style="color:#4d4b4c;" target="_blank" >忘记密码</a></td>
+                              </tr>
+                            </table>                          </td>
+                        </tr>
+                    </table>                  </td>
+                </tr>
+                <tr>
+                  <td height="30"></td>
+                </tr>
+              </table>          </td>
+        </tr>
+      </table>
     </td>
   </tr>
+  <tr>
+    <td height="50" align="center"><img src="<?=$skin_url?>/images/join_bottom_img.png"></td>
+  </tr>
+</form>
 </table>
-<script>
-$(function(){
-    $("#login_auto_login").click(function(){
-        if (this.checked) {
-            this.checked = confirm("开启自动登录功能后系统将会记住您的登录信息.\n\n请勿在网吧等公共场所设备上开启此功能\n\n点击确定开启自动登录功能");
-        }
-    });
-});
-
-function flogin_submit(f)
-{
-    return true;
-}
-</script>
 <!-- } 登录结束 -->
--- oozoobox/skin/member/basic/password_lost.skin.php
+++ oozoobox/skin/member/basic/password_lost.skin.php
@@ -5,52 +5,126 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 add_stylesheet('<link rel="stylesheet" href="'.$skin_url.'/style.css" media="screen">', 0);
 
 ?>
-
-<form class="form-horizontal" role="form" name="fpasswordlost" action="<?php echo $action_url ?>" onsubmit="return fpasswordlost_submit(this);" method="post" autocomplete="off">
-
-	<div class="panel panel-default">
-		<div class="panel-heading"><strong><i class="fa fa-search fa-lg"></i> 회원정보찾기</strong></div>
-		<div class="panel-body">
-			<p class="help-block">
-				회원가입 시 등록하신 이메일 주소를 입력해 주세요. 해당 이메일로 아이디와 비밀번호 정보를 보내드립니다.
-			</p>
-			<div class="form-group has-feedback">
-				<label class="sound_only" for="mb_email"><b>이메일</b><strong class="sound_only">필수</strong></label>
-				<div class="col-xs-10">
-					<input type="text" name="mb_email" id="mb_email" required class="form-control input-sm email" size="30" maxlength="100">
-					<span class="fa fa-envelope form-control-feedback"></span>
-				</div>
-			</div>
-
-			<div class="form-group">
-				<div class="col-xs-10">
-					<?php echo captcha_html(); ?>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div class="text-center" style="margin:15px 0px 0px;">
-		<button type="submit" class="btn btn-color btn-sm">확인</button>
-        <button type="button" class="btn btn-black btn-sm" onclick="window.close();">닫기</button>
-	</div>
-</form>
-
-<script>
-function fpasswordlost_submit(f) {
-    <?php echo chk_captcha_js();  ?>
-
-    return true;
+<style type="text/css">
+<!--
+html { overflow-y: scroll; }
+body,td,th {
+	font-size: 12px; font-family:微软雅黑;
 }
-
-$(function() {
-    var sw = screen.width;
-    var sh = screen.height;
-    var cw = document.body.clientWidth;
-    var ch = document.body.clientHeight;
-    var top  = sh / 2 - ch / 2 - 100;
-    var left = sw / 2 - cw / 2;
-    moveTo(left, top);
-});
-</script>
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	background-image: url(<?=$skin_url?>/images/login_bg.jpg);
+	background-repeat: repeat-x;
+	background-color: #eeeeee;
+}
+/*** 기본버튼 **/
+.btn.btn-color { 
+	border: 1px solid #f47a22; 
+	border-image: none; 
+	color: rgb(255, 255, 255) !important; 
+	background-color: #f5944f; 
+	background-image: none;
+	padding:10px 20px 10px 20px;
+}
+.btn.active.btn-color, .btn.btn-color:hover, .btn.btn-color:focus, .btn.btn-color:active { 
+	border-color: #f47a22; 
+	color: rgb(255, 255, 255) !important; 
+	background-color: #f47a22; 
+	background-image: none; 
+	padding:10px 20px 10px 20px;
+}
+a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:hover {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+.r_input{border:solid 1px #d9d9d9; width:240px; height:31px; color:#bdbdbd;}
+-->
+</style>
+<script type="text/javascript" src="/js/jquery-1.8.3.min.js"></script> 
+<script type="text/javascript"><!--自动检查账号是否被注册-->
+	$(
+	  function()
+	  	{    
+		//账号   jQuery(普通应用时推荐，简单易用)
+    	$("#reg_mb_id").blur(function()
+								 {        //文本框鼠标焦点消失事件
+			 						$.get("./member_ck_id.php?user="+$("#reg_mb_id").val(),null,function(data)   //此处get方式 可换为post方式按需求调整，其他无需修改使用方式一样
+      		 					 	{
+          		  						$("#chk").html(data);   //向ID为chk的元素内添加html代码
+       		 						}
+			 						);
+       	 						}
+						)		
+						
+		//邮箱   jQuery(普通应用时推荐，简单易用)
+    	$("#mb_email").blur(function()
+								 {        //文本框鼠标焦点消失事件
+			 						$.get("./member_ck_id.php?mail="+$("#mb_email").val(),null,function(data)   //此处get方式 可换为post方式按需求调整，其他无需修改使用方式一样
+      		 					 	{
+          		  						$("#email").html(data);   //向ID为chk的元素内添加html代码
+       		 						}
+			 						);
+       	 						}
+						)		
+			
+						
+						     
+		}
+	)
+</script> 
+<table width="990" border="0" cellspacing="0" cellpadding="0" align="center">
+<form class="form-horizontal" role="form" name="fpasswordlost" action="<?php echo $action_url ?>" onsubmit="return fpasswordlost_submit(this);" method="post" autocomplete="off">
+  <tr>
+    <td height="162" colspan="2" valign="top" style="padding-top:20px;"><a href="/"><img src="<?=$skin_url?>/images/join_logo.png" border="0"></a></td>
+  </tr>
+  <tr>
+    <td width="559" height="403" valign="top" background="<?=$skin_url?>/images/join_box_bg.png" style="padding-top:35px;">
+        <table width="420" border="0" cellspacing="0" cellpadding="0" align="center">
+          <tr>
+            <td><font style="font-family:微软雅黑; font-size:18px; color:#888888; font-weight:bold;">找回密码</font></td>
+            <td align="right"></td>
+          </tr>
+        </table>
+        <table width="420" border="0" cellspacing="0" cellpadding="0" align="center">
+          <tr>
+            <td height="55"></td>
+          </tr>
+          <tr>
+            <td height="230" valign="top" style="line-height:25px;">
+            		亲爱的 <strong>用户</strong> 请输入您注册时填写的邮箱地址 <br>系统会自动发给你 账号和临时密码.<br>
+                请输入邮箱地址<br>
+                <input type="text" name="mb_email" id="mb_email" required class="form-control input-sm email" size="30" maxlength="100"><span id="email"></span><br>
+                
+            </td>
+          </tr>
+          <tr>
+            <td height="30"></td>
+          </tr>
+        </table>
+        <table width="420" border="0" cellspacing="0" cellpadding="0" align="center">
+          <tr>
+            <td align="right"><button type="submit" id="btn_submit" class="btn btn-color">找回密码</button>
+	    				<a href="<?php echo G5_URL ?>" class="btn btn-color" role="button">取消</a></td>
+          </tr>
+        </table>
+    </td>
+    <td align="center" valign="top"><img src="<?=$skin_url?>/images/join_font.png"></td>
+  </tr>
+  <tr>
+    <td align="center" height="50"><img src="<?=$skin_url?>/images/join_bottom_img.png"></td>
+    <td></td>
+  </tr>
+</form>
+</table>
 <!-- } 회원정보 찾기 끝 -->
