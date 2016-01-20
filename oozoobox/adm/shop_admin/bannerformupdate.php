ad1c7b63b970162ae6c2c8298cb0bfbf4d83a5a8
HeoGeol <heogeol@naver.com>
2016-01-20 17:39:44 +0900

    배너 기능 수정 배너파일 추가

--- oozoobox/adm/shop_admin/bannerformupdate.php
+++ oozoobox/adm/shop_admin/bannerformupdate.php
@@ -1,5 +1,5 @@
 <?php
-$sub_menu = '500500';
+$sub_menu = '100000';
 include_once('./_common.php');
 
 check_demo();
@@ -9,17 +9,67 @@ if ($W == 'd')
 else
     auth_check($auth[$sub_menu], "w");
 
-@mkdir(G5_DATA_PATH."/banner", G5_DIR_PERMISSION);
-@chmod(G5_DATA_PATH."/banner", G5_DIR_PERMISSION);
 
-$bn_bimg      = $_FILES['bn_bimg']['tmp_name'];
-$bn_bimg_name = $_FILES['bn_bimg']['name'];
 
-if ($bn_bimg_del)  @unlink(G5_DATA_PATH."/banner/$bn_id");
+////图片1 开始///
+$f1=$_FILES['bn_img1'];
+$filetype1=date("YmdHis");
+$fileimg1="1";
+$file_name1 =$filetype1."_".$fileimg1;
+ if($f1['name']){
+   $dest_dir='../../data/banner/';
+
+	 $board_type=strtolower(substr($f1['name'],(strrpos($f1['name'],'.')+1)));
+	 $brd_file1=$file_name1.".".$board_type;
+	  
+	  $dest=$dest_dir.$brd_file1;
+
+	  $r1=move_uploaded_file($f1['tmp_name'],$dest);
+
+  }
+
+if($brd_file1){
+	$imgfile1="$brd_file1";
+	////删除图片1 开始///
+	$file_delete = "../../data/banner/$_POST[b_img1]";   
+	@unlink($file_delete);
+}else{
+	$imgfile1="$_POST[b_img1]";
+}
+
+
+
+////图片2 开始///
+$f2=$_FILES['bn_img2'];
+$filetype2=date("YmdHis");
+$fileimg2="2";
+$file_name2 =$filetype2."_".$fileimg2;
+ if($f2['name']){
+   $dest_dir='../../data/banner/';
+
+	 $board_type=strtolower(substr($f2['name'],(strrpos($f2['name'],'.')+1)));
+	 $brd_file2=$file_name2.".".$board_type;
+	  
+	  $dest=$dest_dir.$brd_file2;
+
+	  $r2=move_uploaded_file($f2['tmp_name'],$dest);
+
+  }
+if($brd_file2){
+	$imgfile2="$brd_file2";
+	////删除图片1 开始///
+	$file_delete = "../../data/banner/$_POST[b_img2]";   
+	@unlink($file_delete);
+}else{
+	$imgfile2="$_POST[b_img2]";
+}
+
+
+
+
 
 if ($w=="")
 {
-    if (!$bn_bimg_name) alert('배너 이미지를 업로드 하세요.');
 
     sql_query(" alter table {$g5['g5_shop_banner_table']} auto_increment=1 ");
 
@@ -34,6 +84,9 @@ if ($w=="")
                     bn_begin_time = '$bn_begin_time',
                     bn_end_time   = '$bn_end_time',
                     bn_time       = '$now',
+										bn_bg_color   = '$bn_bg_color',
+										bn_img1       = '$imgfile1',
+										bn_img2       = '$imgfile2',
                     bn_hit        = '0',
                     bn_order      = '$bn_order' ";
     sql_query($sql);
@@ -52,9 +105,31 @@ else if ($w=="u")
                     bn_new_win    = '$bn_new_win',
                     bn_begin_time = '$bn_begin_time',
                     bn_end_time   = '$bn_end_time',
+										bn_bg_color   = '$bn_bg_color',
+										bn_img1       = '$imgfile1',
+										bn_img2       = '$imgfile2',
                     bn_order      = '$bn_order'
               where bn_id = '$bn_id' ";
     sql_query($sql);
+		
+		if($_POST[bn_img1_del]){
+			$file_delete = "../../data/banner/$_POST[b_img1]";   
+			@unlink($file_delete);
+				$sql = " update {$g5['g5_shop_banner_table']}
+										set bn_img1        = ''
+									where bn_id = '$bn_id' ";
+				sql_query($sql);
+		}
+		
+		if($_POST[bn_img2_del]){
+			$file_delete = "../../data/banner/$_POST[b_img2]";   
+			@unlink($file_delete);
+				$sql = " update {$g5['g5_shop_banner_table']}
+										set bn_img2        = ''
+									where bn_id = '$bn_id' ";
+				sql_query($sql);
+		}
+		
 }
 else if ($w=="d")
 {
