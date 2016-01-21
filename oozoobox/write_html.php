063e72740cf208a88bfc77c65417fee04a0e7e11
KJH <ko3464@oozoo.co.kr>
2016-01-21 16:22:03 +0900

    메인 html 생성변경

--- oozoobox/write_html.php
+++ oozoobox/write_html.php
@@ -8,7 +8,7 @@ $fp = fopen("index.html", "w");
 fwrite($fp, $content);
 fclose($fp);
   echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";
-	echo "<script>alert('HTML생성완료되었습니다!');window.location='/'</script>";
+	echo "<script>alert('HTML생성이 완료 되었습니다!');window.location='/'</script>";
 }else{
 	echo "<script>alert('您不是管理员');window.location='/'</script>";
 }
