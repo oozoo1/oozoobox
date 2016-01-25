c63769f3e614dc2a0195a2b46c0ad648d82a2ba0
MH_VANESSA <sapiens12@nate.com>
2016-01-25 17:20:19 +0900

    메인 투표하러 가기 페이지 및 배송 이미지

---
+++ oozoobox/shop/main_fixbn.php
@@ -0,0 +1,31 @@
+<?php
+include_once('./_common.php');
+
+
+
+
+
+$g5['title'] ='MD商品';
+include_once('./_head.php');
+
+$skin_path = $member_skin_path;
+$skin_url = $member_skin_url;
+?>
+
+
+
+<div id="MainEvent02">
+	<img src="/images/event_vote.png" alt="投票"/>
+    <div class="mainevent_btn">
+    	<a href="/">
+        	<img src="/images/btn_go_home.png" alt="go home 返回首页"/>
+        </a>
+    </div>
+        
+        
+</div>
+
+
+
+        
+<?php  include_once('./_tail.php'); ?>
