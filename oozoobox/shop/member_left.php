e34d9c184e27a909b018310bde478e43c95d987f
HeoGeol <heogeol@naver.com>
2016-01-26 14:50:48 +0900

    마이페이지 left 메뉴 수정

--- oozoobox/shop/member_left.php
+++ oozoobox/shop/member_left.php
@@ -33,15 +33,17 @@
                     <li><a href="/shop/mypage03_3.php">填写商品评论</a></li>
                     <li><a href="/shop/mypage03_4.php">意见返顾</a></li> <!-- CS CENTER에 상품취소 환불등을 건의 할 때-->
                     <li><a href="/shop/mypage03_5.php">举报/投诉 管理</a></li> <!-- CS CENTER에 판매자를 신고할 때-->
+                    <li><a href="/shop/mypage04_3.php">获奖列表</a></li>
                 </ul>  
+              <!--  
             	<h4 class="MM_tit">
                 	<img src="/images/tit_myozbox04.png" alt="我的优惠 OOZOOBOX Event"/>
                 </h4>
                 <ul class="MM_List">
                 	<li><a href="/shop/mypage04_1.php">优惠券</a></li>
                     <li><a href="/shop/mypage04_2.php">积分</a></li>
-                    <li><a href="/shop/mypage04_3.php">获奖列表</a></li>
-                </ul> 
+                    
+                </ul> -->
 			</div>
             <!--e: MypageMenu-->
             <!--s: 고객센터 배너-->
