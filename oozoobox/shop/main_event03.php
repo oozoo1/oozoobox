50d42cd27eefd2abbf513dadfde3a010c13b8652
MH_VANESSA <sapiens12@nate.com>
2016-01-26 15:15:55 +0900

    이벤트 존

--- oozoobox/shop/main_event03.php
+++ oozoobox/shop/main_event03.php
@@ -12,8 +12,6 @@ $skin_url = $member_skin_url;
 	<img src="/images/top_event/top_event01.png" alt="享受大5折优惠"/>
     <img src="/images/top_event/top_event02.png" alt="津津有味”商品打5折"/>
     <img src="/images/top_event/top_event03.png" alt="CJ食品礼品大集合 4折"/>
-
-</div>    
     
     <div class="mainevent_btn">
     	<a href="/">
@@ -21,6 +19,9 @@ $skin_url = $member_skin_url;
         </a>
     </div>     
     
+</div>    
+    
+
        
 <?php  include_once('./_tail.php'); ?>
 
